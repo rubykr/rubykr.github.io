269468a0c357174f4310107dfbf0ef51cf05b06a
Shim Won <marocchino@gmail.com>
2015-01-16 16:42:19 +0900

    Extract Article class

--- _plugins/generators.rb
+++ _plugins/generators.rb
@@ -1,8 +1,6 @@
 require 'jekyll'
 
 class RSSGenerator < Jekyll::Generator
-  ROW_MATCHER = /^-\s*\[(?<title>[^\]]+)\]\((?<url>[^)]+)\){:\s*.article(?<data>(\s*data-\w+="[^"]+")+)\s*}\s*$/
-  DATA_MATCHER = /data-(?<key>\w+)="(?<value>[^"]+)"/
   def generate(site)
     @index_page = site.pages.find { |page| page.name == "index.md" }
     site.pages.each do |page|
@@ -10,33 +8,50 @@ class RSSGenerator < Jekyll::Generator
     end
   end
 
+  private
   def articles
-    unless @articles
-      @articles = []
-      rows.each do |row|
-        article = {}
-        m = row.match(ROW_MATCHER)
-        m[:data].scan(DATA_MATCHER) do |key, value|
-          article[key] = value
-        end
-        set_title(article, m)
-        article["url"] = m[:url]
-        @articles << article
-      end
-    end
-    @articles
+    @articles ||= Article.all(rows)
   end
 
-  private
   def rows
     @index_page.content.split(/#articles\s*}\n\n/).last.split(/## \[동영상\]/).first.split(/\n/)
   end
+end
 
-  def set_title(article, m)
-    if article["tags"].include?("translated")
-      article["title"] = "[번역] #{m[:title]}"
+class Article
+  ROW_MATCHER = /^-\s*\[(?<title>[^\]]+)\]\((?<url>[^)]+)\){:\s*.article(?<data>(\s*data-\w+="[^"]+")+)\s*}\s*$/
+  DATA_MATCHER = /data-(?<key>\w+)="(?<value>[^"]+)"/
+  InvalidRow = Class.new(RuntimeError)
+  attr_reader :info
+
+  def self.all(rows)
+    rows.map { |row| new(row).to_h }
+  end
+
+  def initialize(row)
+    m = row.match(ROW_MATCHER)
+    raise InvalidRow, "invalid row: #{row}" unless m
+    @info = { "url" => m[:url] }
+    set_data(m)
+    set_title(m)
+  end
+
+  def to_h
+    @info
+  end
+
+  private
+  def set_title(m)
+    if @info["tags"].include?("translated")
+      @info["title"] = "[번역] #{m[:title]}"
     else
-      article["title"] = m[:title]
+      @info["title"] = m[:title]
+    end
+  end
+
+  def set_data(m)
+    m[:data].scan(DATA_MATCHER) do |key, value|
+      @info[key] = value
     end
   end
 end
--- spec/generators_spec.rb
+++ spec/generators_spec.rb
@@ -2,18 +2,32 @@ require 'spec_helper'
 describe RSSGenerator do
   subject(:generator) { RSSGenerator.new }
   it { is_expected.not_to be_nil }
+end
 
-  describe "#articles" do
-    before do
-      allow(subject).to receive(:rows).and_return([
+describe Article do
+  describe ".all" do
+    subject(:articles) { Article.all([
         '- [title](https://link.to.article){: .article data-date="2015.01.12" data-tags="translated ruby" data-author="author" }',
         '- [title](https://link.to.article){: .article data-date="2015.01.12" data-tags="ruby" data-author="author" }',
-      ])
+      ]) }
+    it { expect(articles.size).to be(2) }
+  end
+
+  describe "#new" do
+    subject(:article) { Article.new(row) }
+    context 'when translated row' do
+      let(:row) { '- [title](https://link.to.article){: .article data-date="2015.01.12" data-tags="translated ruby" data-author="author" }' }
+      it { expect(subject.to_h["title"]).to eq("[번역] title") }
     end
 
-    it { expect(subject.articles.size).to be(2) }
-    it { expect(subject.articles.first["title"]).to eq("[번역] title") }
-    it { expect(subject.articles.last["title"]).to eq("title") }
+    context 'when normal row' do
+      let(:row) { '- [title](https://link.to.article){: .article data-date="2015.01.12" data-tags="ruby" data-author="author" }' }
+      it { expect(subject.to_h["title"]).to eq("title") }
+    end
 
+    context 'when invalid row' do
+      let(:row) { '' }
+      it { expect { article }.to raise_error Article::InvalidRow }
+    end
   end
 end
